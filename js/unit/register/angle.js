6acac2fc0fbb872f0ac11398466441e6520ccfe7
rounak bastola <rounak bastola>
2016-11-12 00:51:04 -0600

    Added angle

---
+++ js/unit/register/angle.js
@@ -0,0 +1,9 @@
+const BaseUnit = require('../base_unit');
+
+const type = 'angle';
+
+module.exports = () => {
+  BaseUnit.register(type, '', 1);
+  BaseUnit.register(type,'°',57.2958,'rad');
+
+};
--- js/unit/register/speed.js
+++
@@ -1,7 +0,0 @@
-const DerivedUnit = require('../derived_unit');
-
-const type = 'speed';
-
-module.exports = () => {
-  DerivedUnit.register(type, 'm/s', [['length', 'm']], [['time', 's']]);
-};
