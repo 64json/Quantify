fa92f8e6f01d6766bacc6d10eddd76e977ece47f
Jason Park <parkjs814@gmail.com>
2016-11-13 03:37:56 -0600

    fix

--- js/unit/register/matter.js
+++ js/unit/register/matter.js
@@ -3,6 +3,6 @@ const BaseUnit = require('../base_unit');
 const type = 'matter';
 
 module.exports = () => {
-  BaseUnit.register(type, 'moles', 'mol', 1);
-  BaseUnit.register(type, 'atoms', 'atoms', 6.02e23, 'mol');
+  BaseUnit.register(type, 'atoms', 'atom', 1);
+  BaseUnit.register(type, 'moles', 'mol', 6.022140857e23, 'atom');
 };
