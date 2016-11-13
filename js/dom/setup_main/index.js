c7e531239e1fcff405f596655d73b9b5af150665
Jason Park <parkjs814@gmail.com>
2016-11-12 22:46:00 -0600

    add cards

--- js/dom/setup_main/index.js
+++ js/dom/setup_main/index.js
@@ -8,48 +8,61 @@ module.exports = () => {
     if (event.keyCode == 13) {
 
       const unitless = Server.refineLaTeX(mathField.latex());
-      var factor = 1;
       if (isNaN(unitless)) {
         const combinations = Server.search(unitless);
         console.log(combinations);
+        $('.result-container:not(.template)').remove();
         combinations.forEach(combination => {
-          const powers = [];
-          for (const quantities of [combination.derivedQuantities, combination.baseQuantities]) {
-            for (const quantity in quantities) {
-              const unitClass = app.getStandardUnitClass(quantity);
-              factor *= Math.pow(unitClass.QUANTITY, quantities[quantity]);
-              powers.push([unitClass, quantities[quantity]]);
-            }
-          }
-
-          var mulSymbols = [], divSymbols = [];
-
-          for (const power of powers) {
-            if (power[1] > 0) {
-              if (power[1] > 1) {
-                mulSymbols.push(power[0].SYMBOL + '^' + power[1]);
-              } else {
-                mulSymbols.push(power[0].SYMBOL);
-              }
-            } else {
-              if (power[1] < -1) {
-                divSymbols.push(power[0].SYMBOL + '^' + -power[1]);
-              } else {
-                divSymbols.push(power[0].SYMBOL);
-              }
-            }
-          }
-          var latex = unitless.quantity * factor + ' ' + mulSymbols.join(' * ');
-          if (divSymbols.length) {
-            if (divSymbols.length > 1) {
-              latex += ' / (' + divSymbols.join(' * ') + ')';
-            } else {
-              latex += ' / ' + divSymbols[0];
-            }
-          }
-          console.log(latex);
+          renderCombination(unitless, combination);
         });
       }
     }
   });
+};
+
+const renderCombination = (unitless, combination) => {
+  var factor = 1;
+  const powers = [];
+
+  for (const quantities of [combination.derivedQuantities, combination.baseQuantities]) {
+    for (const quantity in quantities) {
+      const unitClass = app.getStandardUnitClass(quantity);
+      factor *= Math.pow(unitClass.QUANTITY, quantities[quantity]);
+      powers.push([unitClass, quantities[quantity]]);
+    }
+  }
+
+  var mulSymbols = [], divSymbols = [];
+
+  for (const power of powers) {
+    if (power[1] > 0) {
+      if (power[1] > 1) {
+        mulSymbols.push(power[0].SYMBOL + '^' + power[1]);
+      } else {
+        mulSymbols.push(power[0].SYMBOL);
+      }
+    } else {
+      if (power[1] < -1) {
+        divSymbols.push(power[0].SYMBOL + '^' + -power[1]);
+      } else {
+        divSymbols.push(power[0].SYMBOL);
+      }
+    }
+  }
+  var latex = unitless.quantity * factor + ' ' + mulSymbols.join('\\cdot ');
+  if (divSymbols.length) {
+    if (divSymbols.length > 1) {
+      latex += ' / (' + divSymbols.join('\\cdot ') + ')';
+    } else {
+      latex += ' / ' + divSymbols[0];
+    }
+  }
+
+  const $templateResultContainer = $('.result-container.template');
+  const $resultContainer = $templateResultContainer.clone();
+  const $answer = $resultContainer.find('.answer');
+  $resultContainer.removeClass('template');
+  $answer.text(latex);
+  var staticMath = MQ.StaticMath($answer[0]);
+  $resultContainer.insertBefore($templateResultContainer);
 };
