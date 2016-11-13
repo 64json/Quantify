107f465ed5ce856271593d16c8e89489d3d89500
rounak bastola <rounak bastola>
2016-11-12 21:10:04 -0600

    fixed prefixes

--- js/unit/register/mass.js
+++ js/unit/register/mass.js
@@ -6,7 +6,7 @@ module.exports = () => {
   BaseUnit.register(type, 'grams', 'g', 1);
   BaseUnit.register(type, 'deci grams', 'dg', 1e-1, 'g');
   BaseUnit.register(type, 'centi grams', 'cg', 1e-2, 'g');
-  BaseUnit.register(type, 'kilo grams', 'kg', 1e3, 'g');
+  BaseUnit.register(type, 'kilo grams', 'Kg', 1e3, 'g');
   BaseUnit.register(type, 'milli grams', 'mg', 1e-3, 'g');
   BaseUnit.register(type, 'nano grams', 'ng', 1e-9, 'g');
   BaseUnit.register(type, 'pico grams', 'pg', 1e-12, 'g');
